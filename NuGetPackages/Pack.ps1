db4ea61a09ad49ec5bfc46ff8afdeb09b4f7f82e
Jonathan Noyola <noyolajonathan@gmail.com>
2019-04-25 17:01:58 -0700

    Add Pipeline target and update nuspecs

--- NuGetPackages/Pack.ps1
+++ NuGetPackages/Pack.ps1
@@ -1,5 +1,12 @@
-$Platforms = @("DesktopGL", "Android", "iOS", "Content.Pipeline.Windows")
-$Version = "1.0.0.0-jnoyola"
+$Packages = @(
+"Framework.DesktopGL",
+"Framework.Android",
+"Framework.iOS",
+"Framework.Content.Pipeline.Windows",
+"Content.Builder"
+)
+
+$Version = "3.7.1.11"
 $Configuration = "Release"
 $Bin = "..\MonoGame.Framework\bin"
 $Output = ".\Output"
@@ -7,13 +14,13 @@ $Output = ".\Output"
 $scriptDir = Split-Path $MyInvocation.MyCommand.Path
 $Bin = Join-Path $scriptDir $Bin
 $Output = Join-Path $scriptDir $Output
-$nugetExe = Join-Path $scriptDir ".\nuget.exe"
+$nugetExe = "nuget.exe"
 
 if(!(Test-Path -Path $Output )){
     New-Item -ItemType directory -Path $Output
 }
 
-foreach ($platform in $Platforms) {
-    $nuspecFile = Join-Path $scriptDir ".\MonoGame.Framework.$platform.nuspec"
+foreach ($package in $Packages) {
+    $nuspecFile = Join-Path $scriptDir ".\MonoGame.$package.nuspec"
 	& $nugetExe pack $nuspecFile -BasePath $Bin -OutputDirectory $Output -Properties "Configuration=$Configuration" -Version $Version
 }
